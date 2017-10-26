aee5cc9558db6035365b261bdbb5692beca892bc
Joao Paulo Amaral Azevedo e Silva <joaopaulosi1@hotmail.com>
2017-10-26 17:36:37 -0200

    boot e telas base

---
+++ index.php
@@ -0,0 +1,24 @@
+
+<?php
+include "modelo.php";
+
+?>
+<br>
+<hr>
+<div>
+<h1>Titulo</h1>
+<hr>
+<p>"Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit..."
+"Não há ninguém que ame a dor por si só, que a busque e queira tê-la, simplesmente por ser dor..."
+
+Lorem ipsum dolor sit amet, consectetur adipiscing elit. In vitae est libero. Aliquam semper est vitae nunc dictum, sit amet elementum mauris pharetra. Nulla luctus ornare lorem, nec lacinia nulla. Integer lobortis convallis varius. Vivamus at efficitur mauris, vitae suscipit augue. Nunc feugiat luctus ligula malesuada faucibus. Pellentesque sit amet arcu ipsum. Ut efficitur porttitor enim, ut posuere mi maximus sit amet. Etiam arcu magna, aliquam sit amet auctor eu, eleifend nec metus.
+
+Cras leo orci, dignissim ut eros non, lacinia sodales libero. Sed sed ligula rhoncus, aliquam ante eget, faucibus est. Sed tortor arcu, rhoncus eget mi ut, congue scelerisque mi. Aenean orci sem, hendrerit ut maximus vel, cursus et libero. Quisque eget tortor venenatis mi posuere fringilla. Ut molestie et dolor vitae luctus. Nunc interdum, purus ut hendrerit euismod, ex odio convallis eros, ac scelerisque mi mauris ut neque. Phasellus sit amet tempor risus. Duis eget tincidunt velit. Donec scelerisque vel mauris sit amet pellentesque.
+
+Proin finibus ultrices tristique. Donec blandit pellentesque quam, vel laoreet sapien. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed sit amet neque commodo enim blandit fringilla. Ut sed massa vitae mauris luctus aliquet quis viverra arcu. Donec et ipsum vel justo tempor dictum. Sed et dui accumsan, aliquet enim a, porttitor nunc.
+
+Sed vestibulum nisl diam, eget posuere odio mattis in. Proin rutrum imperdiet neque id ultricies. Maecenas mollis ac ex id accumsan. In vel sapien sit amet libero hendrerit aliquet. Curabitur tortor sem, faucibus non sollicitudin vel, elementum at ligula. Pellentesque rhoncus erat vel nisi pretium porttitor. Vestibulum viverra massa non arcu hendrerit, in ornare nisi accumsan. Sed faucibus et arcu vitae hendrerit.
+
+Vivamus tempus metus a ex semper semper. Fusce ac mi accumsan mauris tempor congue. Nullam condimentum felis non lectus rhoncus maximus. Mauris vehicula purus elit, nec aliquet augue tristique quis. Praesent ligula ante, pharetra non ipsum vel, efficitur sollicitudin sapien. Pellentesque posuere consequat vestibulum. Praesent quis cursus urna. Etiam vehicula orci ut bibendum laoreet. Nunc dolor orci, luctus sed dui a, molestie lobortis libero. Etiam ac ornare velit, et malesuada neque. Curabitur vestibulum dui mi, luctus semper est pretium nec. Maecenas eu consequat neque. Curabitur laoreet, metus eu lacinia iaculis, lorem elit finibus metus, nec auctor turpis ipsum non nisl. Praesent ultrices vestibulum mollis.</p>
+</div>
+<hr>
---
+++ modelo.php
@@ -0,0 +1,47 @@
+<html>
+
+    <meta charset="utf-8">
+<head>
+    <title>Controle de Patrimonio</title>
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/style.css" rel="stylesheet">
+    <link href="css/dropdown.css" rel="stylesheet">
+    <script src="js/jquery-validade.js" type="text/javascript"></script>
+    <script src="js/js.js" type="text/javascript"></script>
+    <script src="js/jquery.js" type="text/javascript"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js" integrity="sha384-b/U6ypiBEHpOf/4+1nzFpr53nxSS+GLCkfwBdFNTxtclqqenISfwAzpKaMNFNmj4" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/js/bootstrap.min.js" integrity="sha384-h0AbiXch4ZDo7tp9hKZ4TsHbi047NrKGLO3SEJAg45jXxnGIfYzk4Si90RDIqNm1" crossorigin="anonymous"></script>
+    <link href='http://fonts.googleapis.com/css?family=Varela+Round' rel='stylesheet' type='text/css'>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.13.1/jquery.validate.min.js"></script>
+    <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
+</head>
+<body>
+    <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
+    <img alt="Brand" src="logo.png" width="80" height="50">
+    <a class="navbar-brand" href="index.php">   Controle de Patrimônio</a>
+    <button class='navbar-toggler' type='button' data-toggle='collapse' data-target='#navbarCollapse' aria-controls='navbarCollapse' aria-expanded='false' aria-label='Toggle navigation'> <span class='navbar-toggler-icon'></span> </button>
+    <div class='collapse navbar-collapse' id='navbarCollapse'>
+    </div>
+        
+   
+      <div class='dropdown' style="text-align: right,margin-left: 2cm;">
+      <button style="text-align: right,margin-left: 2cm;" class='btn btn-secondary dropdown-toggle' type='button' id='dropdownMenuButton' data-toggle='dropdown' aria-haspopup='true' aria-expanded='false'>
+      Bem vindo Usuário!
+      </button>
+      <div class='dropdown-menu' aria-labelledby='dropdownMenuButton'>
+    	
+        <a class="dropdown-item" href="T03.php">Incluir Usuario</a>
+		<a class="dropdown-item" href="T02.php">Incluir Prédio</a>
+        <a class="dropdown-item" href="T04.php">Incluir Departamento</a>
+        <a class="dropdown-item" href="T05.php">Incluir Salas</a>
+        <a class="dropdown-item" href="T06.php">Incluir Bem</a>
+        <a class="dropdown-item" href="out.php">Listagem dos Bens</a>
+        <a class="dropdown-item" href="logout.php">Logout</a>
+   
+        </div>
+    </div>
+    <p>   </p>
+    </div>
+</nav>
+</html>
+
